ea122434873751724ff57ebdf2d34302a06664bf
Matias Villar <matiase.villar@gmail.com>
2024-07-17 12:13:06 -0600

    allReqConvex

--- convex/allRequests.ts
+++ convex/allRequests.ts
@@ -7,34 +7,38 @@ export const getAllRequests = query({
   handler: async (ctx, args) => {
     const userIdentity = await ctx.auth.getUserIdentity();
     if (!userIdentity) {
+      console.error('User not authenticated');
       throw new ConvexError('User not authenticated');
     }
+    console.log('User identity:', userIdentity);
 
     const currentUser = await getUserByClerkId({
       ctx,
       clerkId: userIdentity.subject,
     });
-
     if (!currentUser) {
+      console.error('User not found');
       throw new ConvexError('User not found');
     }
-    // get all requests where the receiver is the current user
+    console.log('Current user:', currentUser);
+
     const requests = await ctx.db
       .query('requests')
       .withIndex('by_receiver', (q) => q.eq('receiver', currentUser._id))
       .collect();
+    console.log('Requests:', requests);
 
     const senderDetails = await Promise.all(
       requests.map(async (request) => {
         const sender = await ctx.db.get(request.sender);
-
         if (!sender) {
+          console.error('Sender not found for request:', request);
           throw new ConvexError('Sender not found');
         }
-
         return { request, sender };
       })
     );
+    console.log('Sender details:', senderDetails);
 
     return senderDetails;
   },
@@ -45,22 +49,26 @@ export const reqCount = query({
   handler: async (ctx, args) => {
     const userIdentity = await ctx.auth.getUserIdentity();
     if (!userIdentity) {
+      console.error('User not authenticated');
       throw new ConvexError('User not authenticated');
     }
+    console.log('User identity:', userIdentity);
 
     const currentUser = await getUserByClerkId({
       ctx,
       clerkId: userIdentity.subject,
     });
-
     if (!currentUser) {
+      console.error('User not found');
       throw new ConvexError('User not found');
     }
+    console.log('Current user:', currentUser);
 
     const requests = await ctx.db
       .query('requests')
       .withIndex('by_receiver', (q) => q.eq('receiver', currentUser._id))
       .collect();
+    console.log('Requests:', requests);
 
     return requests.length;
   },
